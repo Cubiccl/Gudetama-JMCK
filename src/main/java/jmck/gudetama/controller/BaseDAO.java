c1c91c672ab4acdb8964cb69c7bf1b292aa2424e
Cubiccl <ccl.clement@hotmail.fr>
2017-02-22 19:47:13 +0100

    Base DAO

---
+++ src/main/java/jmck/gudetama/controller/BaseDAO.java
@@ -0,0 +1,20 @@
+package jmck.gudetama.controller;
+
+public class BaseDAO<T>
+{
+
+	public void delete(T object)
+	{}
+
+	public T find(int id)
+	{
+		return null;
+	}
+
+	public void merge(T object)
+	{}
+
+	public void persist(T object)
+	{}
+
+}
